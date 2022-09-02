d260e111627ccb767fb9ce17e6ec7a1dd23db309
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-03 02:35:09 +0600

    news by category completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const loadCategories = () =>{
+const loadCategories = () => {
     fetch("https://openapi.programming-hero.com/api/news/categories")
         .then(response => response.json())
         .then(data => displayCategories(data.data.news_category))
@@ -15,11 +15,11 @@ const displayCategories = (categories) => {
         `;
         categoriesLists.appendChild(list);
     });
-}   
+}
 
 const loadNewsOfCategory = (categoryId, categoryName) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
-    
+
     fetch(url)
         .then(response => response.json())
         .then(data => displayNewsByCategory(data.data, categoryName))
@@ -29,8 +29,50 @@ const loadNewsOfCategory = (categoryId, categoryName) => {
 const displayNewsByCategory = (newsList, categoryName) => {
     console.log(newsList);
     document.getElementById('news-by-category').classList.remove('d-none');
-    document.getElementById('number-of-news').innerText = `${newsList.length ? `${newsList.length} items found for category ${categoryName}`: `No items found for category ${categoryName}!`}`;
-    document.getElementById('news-view-by-category').classList.remove('d-none');
+    document.getElementById('number-of-news').innerText = `${newsList.length ? `${newsList.length} items found for category ${categoryName}` : `No items found for category ${categoryName}!`}`;
+    const displayNewsContainer = document.getElementById('news-view-by-category');
+    displayNewsContainer.classList.remove('d-none');
+    displayNewsContainer.textContent = ``;
+    newsList.forEach(news => {
+        const newsContainer = document.createElement('div');
+        newsContainer.classList.add('col');
+        newsContainer.innerHTML = `
+            <div class="card h-100 flex-md-row p-2">
+                <img src="${news.thumbnail_url}" class="card-img news-image" alt="...">
+                <div class="card-body d-flex flex-column justify-content-between py-4">
+                    <div>
+                        <h5 class="card-title fw-bold">${news.title}
+                        </h5>
+                        <p class="card-text text-secondary mt-3">${news.details.split(" ").length > 50 ? `${news.details.substr(0, news.details.indexOf(" ", 300))}...` : `${news.details}`}
+                        </p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <img src="${news.author.img}" class="rounded-pill img-fluid" alt="" style="width:60px; height:60px">
+                            <div class="ms-3">
+                                <p class="m-0">${news.author.name ? news.author.name : "No data found!"}</p>
+                                <p class="text-muted m-0">${news.author.published_date ? news.author.published_date: "No data found!"}</p>
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <p class="m-0 text-secondary fw-bold"><i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view : "No data found!"}</p>
+                        </div>
+                        <div>
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
+                        <button class="btn border-0 news-text-primary"><i
+                                class="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        displayNewsContainer.appendChild(newsContainer);
+    });
 }
 
 loadCategories();
