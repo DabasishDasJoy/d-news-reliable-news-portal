cc34c5149f34fc61d514f08ca974641a0dd0fe87
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-02 23:28:45 +0600

    fetched category datas

--- js/app.js
+++ js/app.js
@@ -11,10 +11,19 @@ const displayCategories = (categories) => {
     categories.forEach(category => {
         const list = document.createElement('li');
         list.innerHTML = `
-            <a class=" news-text-secondary text-decoration-none" href="#">${category.category_name}</a>
+            <a class=" news-text-secondary text-decoration-none" href="#" onclick="loadNewsOfCategory('${category.category_id}')">${category.category_name}</a>
         `;
         categoriesLists.appendChild(list);
     });
 }   
+
+const loadNewsOfCategory = (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data.data))
+        .catch(error => console.log(error))
+}
 
 loadCategories();
