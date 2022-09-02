69aaaf15dffb8ed4fe1648721e95490ec728f20a
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-03 01:24:33 +0600

    showing card just on category click

--- js/app.js
+++ js/app.js
@@ -11,19 +11,26 @@ const displayCategories = (categories) => {
     categories.forEach(category => {
         const list = document.createElement('li');
         list.innerHTML = `
-            <a class=" news-text-secondary text-decoration-none" href="#" onclick="loadNewsOfCategory('${category.category_id}')">${category.category_name}</a>
+            <a class=" news-text-secondary text-decoration-none" href="#" onclick="loadNewsOfCategory('${category.category_id}', '${category.category_name}')">${category.category_name}</a>
         `;
         categoriesLists.appendChild(list);
     });
 }   
 
-const loadNewsOfCategory = (categoryId) => {
+const loadNewsOfCategory = (categoryId, categoryName) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
     
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data.data))
+        .then(data => displayNewsByCategory(data.data, categoryName))
         .catch(error => console.log(error))
+}
+
+const displayNewsByCategory = (newsList, categoryName) => {
+    console.log(newsList);
+    document.getElementById('news-by-category').classList.remove('d-none');
+    document.getElementById('number-of-news').innerText = `${newsList.length ? `${newsList.length} items found for category ${categoryName}`: `No items found for category ${categoryName}!`}`;
+    document.getElementById('news-view-by-category').classList.remove('d-none');
 }
 
 loadCategories();
