fa02fca3a803f47b69d73d83afad7739a53c0f62
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-03 15:04:45 +0600

    soting completed dynamically

--- js/app.js
+++ js/app.js
@@ -25,14 +25,11 @@ const loadNewsOfCategory = (categoryId, categoryName) => {
 
     fetch(url)
         .then(response => response.json())
-        .then(data => displayNewsByCategory(data.data, categoryName))
+        .then(data => displayNewsByCategory(data.data, categoryName, 'd'))
         .catch(error => console.log(error))
 }
 
-const displayNewsByCategory = (newsList, categoryName) => {
-    // -----Sorting data --------
-    newsList.sort((a, b) => b.total_view - a.total_view);
-
+const displayNewsByCategory = (newsList, categoryName, view) => {
     // ---------showing display news section---------
     document.getElementById('news-by-category').classList.remove('d-none');
 
@@ -40,6 +37,31 @@ const displayNewsByCategory = (newsList, categoryName) => {
     document.getElementById('number-of-news').innerText = `${newsList.length ? `${newsList.length} items found for category ${categoryName}` : `No items found for category ${categoryName}!`}`;
 
     // -------------display news viewing section---------
+    displayAllNewsOfCategory(newsList, view);
+
+    // ------stop spinner----------
+    toggleSpinner(false);
+
+    // ------------Sort by view--------
+    const select = document.getElementById('sort-options');
+    select.addEventListener('change', function (event) {
+        // -----Sorting data --------
+        view = event.target.value;
+        if(view === 'd'){
+            displayAllNewsOfCategory(newsList, event.target.value);
+        }
+        else if (view === 'l') {
+            displayAllNewsOfCategory(newsList.sort((a, b) => a.total_view - b.total_view), event.target.value);
+        }
+        else {
+            displayAllNewsOfCategory(newsList.sort((a, b) => b.total_view - a.total_view), event.target.value);
+        }  
+    })
+
+}
+
+const displayAllNewsOfCategory = (newsList) => {
+
     const displayNewsContainer = document.getElementById('news-view-by-category');
     displayNewsContainer.classList.remove('d-none');
 
@@ -62,7 +84,7 @@ const displayNewsByCategory = (newsList, categoryName) => {
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="d-flex justify-content-center align-items-center">
-                            <img src="${news.author.img}" class="rounded-pill img-fluid" alt="" style="width:60px; height:60px">
+                            <img src="${news.author.img}" class="rounded-circle img-fluid" alt="" style="width:60px; height:60px">
                             <div class="ms-3">
                                 <p class="m-0">${news.author.name ? news.author.name : "No data found!"}</p>
                                 <p class="text-muted m-0">${news.author.published_date ? news.author.published_date : "No data found!"}</p>
@@ -86,23 +108,57 @@ const displayNewsByCategory = (newsList, categoryName) => {
         `;
         displayNewsContainer.appendChild(newsContainer);
     });
-
-    // ------stop spinner----------
-    toggleSpinner(false);
 }
 
 const loadNewsDetails = (news_id) => {
     const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
     fetch(url)
         .then(response => response.json())
-        .then(data=>displayNewsDetails(data.data[0]))
+        .then(data => displayNewsDetails(data.data[0]))
         .catch(error => console.log(error))
 }
 
 const displayNewsDetails = (details) => {
     console.log(details);
     // -----Show modal title---
-    document.getElementById('newDetailsModalLabel').innerText = details.title;
+    // document.getElementById('newDetailsModalLabel').innerText = details.title;
+    const newBody = document.getElementById('news-body');
+    newBody.innerHTML = `
+        <div class="h-100 d-flex flex-column p-2">
+            <div class="text-center">
+                <img src="${details.image_url}" class="card-img" alt="..." style="width: auto; height:90vh">
+            </div>
+
+            <div class="card-body d-flex flex-column justify-content-between py-4">
+                <div>
+                    <h5 class="card-title fw-bold">${details.title}
+                    </h5>
+                    <p class="card-text text-secondary mt-3"> ${details.details}}
+                    </p>
+                </div>
+                <div class="d-flex justify-content-between align-items-center my-3">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <img src="${details.author.img}" class="rounded-circle img-fluid" alt="" style="width:60px; height:60px">
+                        <div class="ms-3">
+                            <p class="m-0">${details.author.name ? details.author.name : "No data found!"}</p>
+                            <p class="text-muted m-0">${details.author.published_date ? details.author.published_date : "No data found!"}</p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <p class="m-0 text-secondary fw-bold"><i class="fa-regular fa-eye"></i> ${details.total_view ? details.total_view : "No data found!"}</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-star-half-stroke"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    `;
 }
 
 const toggleSpinner = (isSpinning) => {
